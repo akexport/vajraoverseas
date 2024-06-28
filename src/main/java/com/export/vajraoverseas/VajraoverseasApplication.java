4cadd18e7666695a42b95858551903e83788d5ee
akexport <akexport23@gmail.com>
2024-06-28 19:52:09 +0530

    Started application code

---
+++ src/main/java/com/export/vajraoverseas/VajraoverseasApplication.java
@@ -0,0 +1,13 @@
+package com.export.vajraoverseas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VajraoverseasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VajraoverseasApplication.class, args);
+	}
+
+}
